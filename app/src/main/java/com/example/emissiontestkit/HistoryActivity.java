53d18d5a493043e099a69f289b0e69d81fdd25d0
lnztumala <lnztumala1@gmail.com>
2024-04-07 22:15:13 +0800

    Home 50% - need to resize activity buttons

--- app/src/main/java/com/example/emissiontestkit/HistoryActivity.java
+++ app/src/main/java/com/example/emissiontestkit/HistoryActivity.java
@@ -1,6 +1,8 @@
 package com.example.emissiontestkit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,23 @@ public class HistoryActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void onHomeClicked(View view) {
+        // Handle emission LinearLayout click here
+        Intent intent = new Intent(HistoryActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void onEmissionClicked(View view) {
+        // Handle emission LinearLayout click here
+        Intent intent = new Intent(HistoryActivity.this, EmissionActivity.class);
+        startActivity(intent);
+    }
+    public void onSettingsClicked(View view) {
+        // Handle emission LinearLayout click here
+        Intent intent = new Intent(HistoryActivity.this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
